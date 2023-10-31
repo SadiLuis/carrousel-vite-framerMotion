c9adf616c397e79572ed237b98798ca20023b4a4
SadiLuis <sadiluisrueda.94@gmail.com>
2023-10-31 18:06:03 -0300

    Nuevos cambios

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <header className="bg-white shadow-white shadow-md text-white py-4">
         <div className="container mx-auto">
             <h1 className="text-4xl text-black font-semibold">
-                Welcome to this awesome carrousel page!
+                Welcome !!
             </h1>
         </div>
     </header>
